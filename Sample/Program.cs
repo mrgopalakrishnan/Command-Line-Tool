d134b47388ad8805b3f5dfd9f8c553892fcf28be
RANGASAMY Gopalakrishnan <gopalakrishnan.rangasamy@fime.com>
2021-06-16 21:01:09 +0530

    first commit

---
+++ Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Cli cli = new()
+            {
+                Introduction = "my intro",
+                PromptText = "CLI"
+            };
+            cli.RegisterCommand("M1", method1);
+            cli.SetCancellationKeys(new() { "exit" });
+            cli.Start();
+        }
+        static void method1(params string[] args) => args.ToList().ForEach(v => Console.WriteLine($"type: {v.GetType()}, {v}"));
+    }
+}
---
+++ Unittests/CliTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Unittests
+{
+    public class CliTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
